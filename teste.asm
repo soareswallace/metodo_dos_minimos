9d4320ef0319c2250377f00460bbbfd53d7621dc
Wallace Junior <wallace@MacBook-Pro-de-Wallace.local>
2017-05-12 22:30:15 -0300

    Descriçao do Objetivo de teste.asm

--- teste.asm
+++ teste.asm
@@ -1,3 +1,6 @@
+;assembly de test para calcular a distancia entre dois pontos
+;para ajudar no aprendizado
+
 SECTION .data
 	x dq 1.0
 	y dq 2.0
@@ -9,3 +12,5 @@ global main
 main:
 	fld qword[x]
 	fld qword[x1]
+	fsubp st1, st0
+	fld 
