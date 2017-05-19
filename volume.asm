00c19b2c807d6b3011f1aa783660e9c6ca5bb358
Wallace <soareswallace@icloud.com>
2017-05-18 23:45:50 -0300

    Aprendi essa porra

--- volume.asm
+++ volume.asm
@@ -1,13 +1,14 @@
-org 0x7c00
-jmp 0x0000:main
+
 extern printf
 
 
 section .data
     r dq 3.0
     h dq 4.0
+    resposta dq 0.0
+    str_resposta: db 'Volume eh = %f' , 10,0
 section .text
-global main
+    global main
 main:
     fld qword[r]
     fld qword[r]
@@ -16,8 +17,16 @@ main:
     fmulp st1, st0
     fldpi
     fmulp st1, st0
+    fst qword [resposta]
+    push dword [resposta+4]
+    push dword [resposta]
+    push dword str_resposta
+    call printf
+    add esp, 12
+    call fim
 
 
 fim:
-times 510-($-$$) db 0
-dw 0xAA55
+    mov eax, 1
+    mov ebx, 0
+    int 80h
